0efa8886d57f047e362c2792cc2545db6b94c6fd
aythp <ayznk9@gmail.com>
2025-03-12 17:34:02 +0000

    feat: update navigation for logged-in users and enhance HomePage layout

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -19,26 +19,14 @@ export default function LoginPage() {
   const handleLoginSubmit = (e) => {
     e.preventDefault();
     const requestBody = { email, password };
-
-    // Send a request to the server using axios
-    /* 
-    axios.post(`${process.env.REACT_APP_SERVER_URL}/auth/login`)
-      .then((response) => {})
-    */
-
-    // Or using a service
+  
     authService
       .login(requestBody)
       .then((response) => {
-        // If the POST request is successful store the authentication token,
-        // after the token is stored authenticate the user
-        // and at last navigate to the home page
         storeToken(response.data.authToken);
         authenticateUser();
-        navigate("/profile");
       })
       .catch((error) => {
-        // If the request resolves with an error, set the error message in the state
         const errorDescription = error.response.data.message;
         setErrorMessage(errorDescription);
       });
